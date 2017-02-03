c9f5ad6192d5c362eca84d1bafec4d6201714e5b
Chris Bell <bellcc@miamioh.edu>
2017-02-03 10:23:13 -0500

    control panel on loop

--- control.py
+++ control.py
@@ -1,38 +1,34 @@
 #!/usr/bin/python
 import sys
 from communication import updateBanner, getBanner
+from assets import loadLetters
 
-def printBreak():
+def printMessage(message):
+    print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+    print message
     print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
 
-def displayOptions(code):
-    if code == 1:
-        printBreak()
-        print "Invalid option."
-        printBreak()
+def displayOptions():
+    while True:
+        print "(1) Update Banner Text"
+        print "(2) Retrieve Banner Text"
+        print "(3) Simulate Banner"
+        print "(4) Exit"
 
-    print "(1) Update Banner Text"
-    print "(2) Retrieve Banner Text"
-    print "(3) Simulate Banner"
-    print "(4) Exit"
-    printBreak()
+        response = raw_input("Please enter an option: ")
+        response = int(response)
 
-    response = raw_input("Please enter an option: ")
-    response = int(response)
+        if response == 1:
+            newBanner = raw_input("Please enter the new banner: ")
+            updateBanner(newBanner)
+        elif response == 2:
+            banner = getBanner()
+            printMessage("Current Banner: " + banner)
+        elif response == 3:
+            print "Simulate"
+        elif response == 4:
+            sys.exit()
+        else:
+            printMessage("Invalid Input. Try Again.")
 
-    if response == 1:
-        newBanner = raw_input("Please enter the new banner: ")
-        updateBanner(newBanner)
-        displayOptions(0)
-    elif response == 2:
-        banner = getBanner()
-        print "Current Banner: " + banner
-        displayOptions(0)
-    elif response == 3:
-        print "Simulate"
-    elif response == 4:
-        sys.exit()
-    else:
-        displayOptions(1)
-
-displayOptions(0)
+displayOptions()
